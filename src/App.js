780813749687f7c10afb7007c680579319752cb7
Minh Nguyen <tylum123@gmail.com>
2024-09-10 08:56:08 +0700

    navigator
    thêm thanh nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/login";
+import Navigator from "./components/Navigator";
 
 function App() {
   return (
     <div className="App">
+      <Navigator />
       <Login />
     </div>
   );
