00cdfb80cf115cbcabb705c5fd560895bd8f5e14
ThinhNguyenrt <ThinhNPSE183518@fpt.edu.vn>
2024-10-21 11:52:17 +0700

    Change style for pet page
    Thay đổi layout cho pet trong shelter dashboard

--- FE/Pawfriend/src/pages/Shelter/Shelterpet/index.jsx
+++ FE/Pawfriend/src/pages/Shelter/Shelterpet/index.jsx
@@ -6,30 +6,38 @@ const Shelterpet = () => {
     return (
         <div className="pet-page">
             <h2 className="title">Add New Pet</h2>
-            <form className="pet-form">
-                <label>
-                    Name:
-                    <input type="text" placeholder="Name" name="name" required />
-                </label>
-                <label>
-                    Type:
-                    <input type="text" placeholder="Type" name="type" required />
-                </label>
-                <label>
-                    Breed:
-                    <input type="text" placeholder="Breed" name="breed" required />
-                </label>
-                <label>
-                    Age:
-                    <input type="text" placeholder="Age" name="age" required />
-                </label>
-                <label>
-                    Date:
-                    <input type="date" name="date" required />
-                </label>
-                <button type="button" className="upload-btn">Upload Image</button>
-                <button type="submit" className="submit-btn">Submit</button>
-            </form>
+            
+            <div className="form-and-image">
+                <form className="pet-form">
+                    <div className="form-container">
+                        <label>
+                            Name:
+                            <input type="text" placeholder="Name" name="name" required />
+                        </label>
+                        <label>
+                            Type:
+                            <input type="text" placeholder="Type" name="type" required />
+                        </label>
+                        <label>
+                            Breed:
+                            <input type="text" placeholder="Breed" name="breed" required />
+                        </label>
+                        <label>
+                            Age:
+                            <input type="text" placeholder="Age" name="age" required />
+                        </label>
+                        <label>
+                            Date:
+                            <input type="date" name="date" required />
+                        </label>
+                        <button type="button" className="upload-btn">Upload Image</button>
+                        <button type="submit" className="submit-btn">Submit</button>
+                    </div>
+                </form>
+                <div className="pet-image-container">
+                    <img src={ImagePet} alt="Pet" className="pet-image" />
+                </div>
+            </div>
 
             <div className="search-area">
                 <div className="search-input-container">
